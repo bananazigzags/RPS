1d5c0da3b2b61db7a1ad61938976235281dd60a5
bananazigzags <bananazigzags@gmail.com>
2021-07-26 00:30:34 +0700

    first version

---
+++ rockpaper.js
@@ -0,0 +1,69 @@
+const plays = ['Rock', 'Paper', 'Scissors'];
+
+function computerPlay() {
+    return plays[Math.floor(Math.random() * plays.length)];
+}
+
+
+function playRound(playerSelection, computerSelection) {
+
+    console.log("Computer's play is " + computerSelection + ". Your play is " + playerSelection)
+    
+    if (playerSelection == computerSelection) {
+        console.log ("It's a tie");
+        return "Tie";
+    } 
+    if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            console.log ("You lose! Paper beats Rock");
+            return "Computer";
+        } else {
+            console.log ("You win! Rock beats Scissors");
+            return "Player";
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'rock') {
+            console.log ("You win! Paper beats Rock");
+            return "Player";
+        } else {
+            console.log ("You lose! Scissors beats Paper");
+            return "Computer";
+        }
+    } else if (playerSelection == "scissors") {
+        if (computerSelection == 'rock') {
+            console.log ("You lose! Rock beats Scissors");
+            return "Computer";
+        } else {
+            console.log ("You win! Scissors beats Paper");
+            return "Player";
+        }
+    }
+    
+}
+
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+
+        let playerSelection = prompt("What's your play?").toLowerCase();
+        let computerSelection = computerPlay().toLowerCase;
+        
+        let roundWinner = playRound(playerSelection, computerSelection);
+        if (roundWinner == "Computer") {
+            computerScore += 1;
+        } else if (roundWinner == "Player") {
+            playerScore += 1;
+        } 
+        console.log("Player: " + playerScore + "\nComputer: " + computerScore);
+    }
+
+    if (playerScore > computerScore) {
+        console.log("You won!")
+    } else if (computerScore < playerScore) {
+        console.log("Computer won!") 
+    } else console.log("You tied!");
+}
+
+game();
